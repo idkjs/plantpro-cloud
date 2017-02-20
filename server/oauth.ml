62d7071c4a3c0727a3cdaaf70bdf9ddbd1891486
chrismamo1 <christophermcalpine@gmail.com>
2017-02-20 13:14:35 -0700

    add skeleton for oauth login

---
+++ server/oauth.ml
@@ -0,0 +1,15 @@
+open Opium.Std
+open Lwt.Infix
+
+let service_get_oauth = get "/" (fun req ->
+  Lwt.return (`String "not yet implemented")
+  >>| respond)
+
+let _ =
+  let static =
+    Middleware.static ~local_path:"../client" ~uri_prefix:"/static"
+  in
+  App.empty
+  |> service_get_oauth
+  |> middleware static
+  |> App.run_command
