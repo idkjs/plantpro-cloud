9774e3b0a887d24e1048f0e9768c5962c00d43ba
chrismamo1 <christophermcalpine@gmail.com>
2017-03-26 23:30:24 -0600

    more dependencies means more AWESOME, right?

---
+++ server/tests/test-server.js
@@ -0,0 +1,22 @@
+// this file shamelessly stolen from the Nightmare GitHub page
+var Nightmare = require('nightmare');
+var expect = require('chai').expect; // jshint ignore:line
+
+describe('test account creation', function() {
+    this.timeout('30s');
+    var nightmare = Nightmare();
+
+    it('try to make an account first', function(done) {
+        nightmare
+            .goto('http://localhost:8080/static/index.html')
+            .click('body > div.topright > div.userButtons > a')
+            .type('div.signup-container input[name="username"]', 'cunt')
+            .type('div.signup-container input[name="password1"]', 'yourmum')
+            .type('div.signup-container input[name="password2"]', 'yourmum')
+            .click('div.signup-container input[type="submit"]')
+            .wait(2000)
+            .end()
+            .then(result => {done()})
+            .catch(done);
+    });
+});
