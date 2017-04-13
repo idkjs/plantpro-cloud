a6412e61d887a714c5ed948b519d74c3cea130e3
Johnathan Smith <johnathan.smith@student.nmt.edu>
2017-04-12 21:41:44 -0600

    lists plants under groups and under all/ungrouped

--- client/controlpanel/js/dashboard.js
+++ client/controlpanel/js/dashboard.js
@@ -2,6 +2,54 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 
+
+class PlantListing extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log(props);
+        this.state = {
+            plants: [],
+            props: props,
+        };
+    }
+    
+    componentDidMount() {
+        var url = `/get-devices/${this.state.props.username}/${this.state.props.groupname}`;
+        console.log(this.state);
+        console.log(url);
+        axios.get(url)
+            .then(res => {
+                var plants = [];
+                for (var p in res.data) {
+                    if (p.group.name === this.state.props.groupname) 
+                        plants.push(p);
+                }
+                console.log(plants);
+                this.setState({plants: plants});});
+    }
+
+    render() {
+        return (
+            {
+                this.state.plants.map((plant) => {
+                    return (
+                        <li key={plant.name}>
+                            <a href=""><p>{plant.name}</p>
+                                <button key={plant.name} onclick="" class="glyphicon glyphicon-cog pull-right" style="border:none;background-color:trans parent;padding-top:3px;">
+                                </button>
+                            </a>
+                        </li>
+                    );
+                })
+            }
+
+        );
+    }
+}
+
+
+
+
 class GroupsListing extends React.Component {
   constructor(props) {
     super(props);
@@ -31,9 +79,10 @@ class GroupsListing extends React.Component {
             return (
               <li className="dropdown" key={group.name}>
                 <a href="#" className="dropdown-toggle" data-toggle="dropdown">
-                  {group.name}
+                  <p>{group.name}</p>
                   <span className="caret"></span>
                 </a>
+                <PlantListing username={this.state.props.username} groupname={group.name} />
               </li>)})
         }
       </ul>);
@@ -41,50 +90,11 @@ class GroupsListing extends React.Component {
 }
 
 
-class PlantListing extends React.Component {
-    constructor(props) {
-        super(props);
-        console.log(props);
-        this.state = {
-            plants: [],
-            props: props,
-        };
-    }
-    
-    componentDidMount() {
-        var url = `/get-devices/${this.state.props.username}`;
-        console.log(this.state);
-        console.log(url);
-        axios.get(url)
-            .then(res => {
-                var plants = res.data;
-                console.log(plants);
-                this.setState({plants: plants});});
-    }
-
-    render() {
-        return (
-            { 
-                this.state.plants.map((plant) => {
-                    return (
-                        <li key={plant.name}>
-                        <a href="">
-                        <button key={plant.name} onclick="" class="glyphicon glyphicon-cog pull-right" style="border:none;background-color:trans parent;padding-top:3px;">
-                        </button>
-                        </a>
-                        </li>
-                    );
-                })
-            }
-
-        );
-    }
-}
 
 ReactDOM.render(
   <GroupsListing username={username} />,
   document.getElementById('groupsListContainer'));
 
 ReactDOM.render(
-  <PlantListing username={username} />,
+  <PlantListing username={username} groupname={'ungrouped'} />,
   document.getElementByID('plantListContainer'));
