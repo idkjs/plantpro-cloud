18db083980960185b5f97f18612b55c93c0ba0f8
jsmith <jsmith@cs.nmt.edu>
2017-04-26 22:36:59 -0600

    stuff probably won't work, but its a jumping off point

--- client/controlpanel/js/dashboard.js
+++ client/controlpanel/js/dashboard.js
@@ -3,12 +3,116 @@ import React from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
 
+class RenamePlant extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            groups: [],
+            props: props,
+            newPlantName: ""
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({newPlantName: event.target.value});
+    }
+
+    handleSubmit(event) {
+        var url = "/rename-plant";
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" className="form-control" name="groupName1" placeholder="new plant name" value={this.state.value} onChange={this.handleChange} />
+                <button id="renamebutton1" type="submit" className="btn btn-success">Rename Plant</button>
+        	</form>
+        );
+    }
+}
+
+
+class ChangeGroup extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            groups: [],
+            props: props,
+            newGroup: "",
+            outputTray: <div></div>
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({newGroup: event.target.value});
+    }
+    handleSubmit(event) {
+        var url = "/change-group";
+        axios.post(url, /* rm from group ... None */
+            { plant:this.state.props.plant.id
+            , group: ["None"]
+            })
+        axios.post(url, /* add to group ... Some */
+            {  plant:this.state.props.plant.id
+             , group: ["Some", this.state.newGroup]
+            })
+        .then(res => {
+            var style = {
+                paddingLeft: "1.5em"
+            };
+            this.setState({
+                response: res.status,
+                props: this.state.props,
+                outputTray: <span style={style}>Done!</span>});})
+            .catch(res => {
+                var style = {
+                    color: "red",
+                    paddingLeft: "1.5em"
+                };
+                this.setState({
+                    response: 200,
+                    props: this.state.props,
+                    outputTray: <span style={style}>Failed!</span>
+                });});
+    }
+
+    componentDidMount() {
+        var url = `/get-groups/${this.state.props.username}`;
+        console.log("url: " + url);
+        axios.get(url)
+            .then(res => {
+                var groups = res.data;
+                console.log(groups);
+                this.setState({groups: groups});});
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <select value={this.state.value} onChange={this.handleChange}>
+                    <option value="" disabled selected>Select a new group</option>
+                    {
+                        this.state.groups.map((group) {
+                            return (
+                                <option value={group.id} id={group.name}>{group.name}</option>
+                            );
+                        })
+                    }
+            	</select>
+            	<button className="btn btn-default" type="button" id="menu1">Move Plant</button>
+            </form>
+        );
+    }
+}
 
 class PlantListing extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            plants: [],
             props: props,
         };
     }
@@ -33,40 +137,47 @@ class PlantListing extends React.Component {
     }
 
     render() {
-        console.log(this.state.plants.length);
-        var buttonStyle = {
-            border: "none",
-            backgroundColor: "transparent",
-            paddingTop: "13px"
-        };
-        var pStyle = {
-            color: "#bcbcbc",
-            padding: "3px 20px",
-        };
-        var contents;
-        if (this.state.plants.length == 0) {
-            contents = (
-                <li><p style={pStyle}>No Plants in Group</p></li>
-            );
+        var pStyle = {paddingLeft: "25px",};
+
+        if (this.state.plants.length < 1) {
+            return (<li style=pStyle><p>Empty Group</p></li>);
         }
-        else {
-            contents =
-            this.state.plants.map((plant) => {
-                return (
-                    <li key={plant.name}>
-                        <button onclick="location.href=''" className="glyphicon glyphicon-cog pull-right" style={buttonStyle}></button>
-                        <a href=""> {plant.name} </a>
+        else { return (
+            <li>
+                <script>
+                    $(document).ready(function() {
+                        $({'#cog' + this.plantname}).click(function () {
+                            $({'#settings' + this.plantname}).slideToggle();
+                        });
+                    });
+                </script>
+                <ul className="nav innerMenu">
+                    <li>
+                        <button id={'cog' + this.state.props.plantname} type="button" className="sets glyphicon glyphicon-cog pull-right" style="border:none;background-color:transparent;padding-top:13px;"><!----></button>
+                        <a href="#">{this.state.props.plantname}</a>
                     </li>
-                );
-            });
-        }
-        return (
-            <ul className="nav">
-            {
-                contents
-            }
-            </ul>
-        );
+                    <li>
+                        <div id={'settings' + this.plantname} className="content" style="display: none; border-right: solid; border-width: 1px; padding-right: 20px; font-weight: bold; text-align: right;">
+                            <a href="#" style="border-style:none;" onclick="showElement({this.plantname + 'Rename'})">Rename</a>
+                            <div id={this.plantname + 'Rename'} style="display: none;">
+                                <br/>
+                                <RenamePlant username={this.state.props.username} plantname={this.state.props.plant} />
+                            </div>
+                            <br/>
+                            <a href="#" style="border-style:none;" onclick="showElement({'' + this.plantname + 'ChangeGroup'})">Change Group</a>
+                            <div id={this.plantname + 'ChangeGroup'} style="display: none;">
+                                <div>
+                                    <ChangeGroup username={this.state.props.username} groupname={this.state.props.groupname} />
+                                </div>
+                            </div>
+                            <br/>
+                            <div><a href="#" style="border-style:none;" onClick="deletionConfirmation({this.plantname})">Delete Plant</a></div>
+                            <br/>
+                        </div>
+                    </li>
+                </ul>
+            </li>
+        );}
     }
 }
 
@@ -91,19 +202,19 @@ class GroupsListing extends React.Component {
 
     render() {
         return (
-            <ul className="nav">
-                {
-                    this.state.groups.map((group) => {
-                        return (
-                            <li className="dropdown" key={group.name}>
-                                <a href="#" className="dropdown-toggle" data-toggle="dropdown">
-                                    <p>{group.name}</p>
-                                    <span className="caret"></span>
-                                </a>
+            {
+                this.state.groups.map((group) {
+                    return(
+                        <li className="dropdown keep-open menuElement" >
+            	            <a href="#" className="dropdown-toggle" data-toggle="dropdown">{group.name} <span className="caret"><!----></span></a>9
+                	        <ul className="menuDrop dropdown-menu keep-open" role="menu">
                                 <PlantListing username={this.state.props.username} groupname={group.name} />
-                            </li>);})
-                }
-                </ul>);
+                	        </ul>
+                        </li>
+                    );
+                })
+            }
+        );
     }
 }
 
