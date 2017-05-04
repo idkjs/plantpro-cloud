9500acba06325d7e85d723cef8da2d567c4cc80f
chrismamo1 <christophermcalpine@gmail.com>
2017-05-04 03:51:56 -0600

    fixed device id handling

--- client/controlpanel/js/stage.js
+++ client/controlpanel/js/stage.js
@@ -9,9 +9,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
 
-console.log(Chart);
-var LineChart = Chart.Line;
-
 function getCookie(name) {
     var value = "; " + document.cookie;
     var parts = value.split("; " + name + "=");
