d0ec3c151881ea3595e5e041cd0f0f130ff03e07
chrismamo1 <christophermcalpine@gmail.com>
2017-05-05 22:00:34 -0600

    add testing skeleton for server

--- client/tests/test-client.js
+++ client/tests/test-client.js
@@ -11,10 +11,10 @@ describe('test account creation', function() {
     it('try to make an account first', function(done) {
         nightmare
             .goto('http://localhost:8080/index.html')
-            .click('body > div.topright > div.userButtons > a')
-            .type('div.signup-container input[name="username"]', 'cunt')
-            .type('div.signup-container input[name="password1"]', 'yourmum')
-            .type('div.signup-container input[name="password2"]', 'yourmum')
+            .click('body > div.topright > div.userButtons:nth-child(1) > a')
+            .type('div.signup-container input[name="username"]', 'someUser')
+            .type('div.signup-container input[name="password1"]', 'somePassword')
+            .type('div.signup-container input[name="password2"]', 'somePassword')
             .click('div.signup-container input[type="submit"]')
             .wait(2000)
             .end()
---
+++ server/test_server.ml
@@ -0,0 +1,45 @@
+type make_account_req =
+  { username: string [@post]
+  ; password1: string [@post]
+  ; password2: string [@post]
+  ; email: string [@post]
+  }
+  [@@deriving netblob
+    { url = "http://localhost:8080/create-account"
+    ; meth = `Post
+    ; format = `Text
+    }]
+
+type login_req =
+  { username: string [@post]
+  ; password: string [@post]
+  }
+  [@@deriving netblob
+    { url = "http://localhost:8080/login"
+    ; meth = `Post
+    ; format = `Text
+    }]
+
+let test_account () =
+  let%lwt (code, body, cookies) =
+    netblob_post_make_account_req
+      ~username:"someUser"
+      ~password1:"somePassword"
+      ~password2:"somePassword"
+      ~email:"john.mcalpine@student.nmt.edu"
+      ()
+  in
+  let%lwt () = Lwt_io.printf "Body: %s\n" body in
+  let%lwt (code, body, cookies) =
+    netblob_post_login_req
+      ~username:"someUser"
+      ~password:"somePassword"
+      ()
+  in
+  let (k, v) = List.hd cookies in
+  let%lwt () = Lwt_io.printf "Body: %s\n" body in
+  Lwt.return ()
+
+let () =
+  Lwt_main.run (test_account ())
+
